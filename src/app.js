1c270012b794a5b2647d19064e441e931e293d70
pod2004 <dioppaomar76@gmail.com>
2026-07-23 21:52:34 +0000

    Modification du message

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD !');
+  res.send('pink CI/CD !');
 });
 app.get('/bonjour', (req, res) => {
   res.send('Bonjour tout le monde');
